9d17eff1e93309040ed2f1a7d5fe973f57b510e2
somename <someemail>
2015-09-30 16:06:04 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -10,54 +10,55 @@ public class SudokuVerifier {
 		//String[] strArray ;
 		if(strArray.length() != 81){
 			return -5;
-		}
-		
-		
-		for(int i=0; i<=strArray.length(); i++){
-			char str = 0;
-			str = strArray.charAt(i);
-			int num = (int)str;
-			int repeatNum = 0;
-			
-			if(num < 1){
-				return -1;
-			}else{
+		}else{
+			for(int i=0; i<=strArray.length(); i++){
+				char str = 0;
+				str = strArray.charAt(i);
+				int num = (int)str;
+				int repeatNum = 0;
 				
-				for(int j=i+1; j<=(strArray.length()-1); j++){
-					char str2 = 0;
-					str2 = strArray.charAt(j);
-					int num2 = (int)str2;
+				if(num < 1){
+					return -1;
+				}else{
 					
-					if(num2 == num){
-						repeatNum++;
-					}
-					
-					if(i==10 && repeatNum>0){
-						return -2;
-					}else{
-						if(i==19 && repeatNum>0){
+					for(int j=i+1; j<=(strArray.length()-1); j++){
+						char str2 = 0;
+						str2 = strArray.charAt(j);
+						int num2 = (int)str2;
+						
+						if(num2 == num){
+							repeatNum++;
+						}
+						
+						if(i==10 && repeatNum>0){
 							return -2;
 						}else{
-							if(i==28 && repeatNum>0){
+							if(i==19 && repeatNum>0){
 								return -2;
 							}else{
-								if(i==37 && repeatNum>0){
+								if(i==28 && repeatNum>0){
 									return -2;
 								}else{
-									if(i==46 && repeatNum>0){
+									if(i==37 && repeatNum>0){
 										return -2;
 									}else{
-										if(i==55 && repeatNum>0){
+										if(i==46 && repeatNum>0){
 											return -2;
 										}else{
-											if(i==64 && repeatNum>0){
+											if(i==55 && repeatNum>0){
 												return -2;
 											}else{
-												if(i==73 && repeatNum>0){
+												if(i==64 && repeatNum>0){
 													return -2;
 												}else{
-													if(i==82 && repeatNum>0){
+													if(i==73 && repeatNum>0){
 														return -2;
+													}else{
+														if(i==82 && repeatNum>0){
+															return -2;
+														}else{
+															return 0;
+														}
 													}
 												}
 											}
@@ -66,13 +67,16 @@ public class SudokuVerifier {
 								}
 							}
 						}
+						
+						
+						
 					}
-					
-					
-					
 				}
-			}break;
+			}
 		}
+		
+		
+		
 		
 		
 		
