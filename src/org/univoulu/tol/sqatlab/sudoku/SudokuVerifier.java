5604f118c26a98701cb7263e91ec00280e104c5b
gillrena <yiwang@MIKROTILAT-124.student.yo.oulu.fi>
2015-10-01 17:02:14 +0300

    Separate numbers function implementation

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -5,37 +5,82 @@ public class SudokuVerifier {
 	public int verify(String candidateSolution) {
 		
 		String strArray = new String(candidateSolution);
-		
+		int result = 0;
 
 		//String[] strArray ;
 		if(strArray.length() != 81){
-			return -5;
+			result = -5;
 		}else{
-			return 0;
-		}
-		
-		// returns 1 if the candidate solution is correct
-		//return 0;
-	}
-	
-	public void setRows(String candidateSolution){
-		String strArray = new String(candidateSolution);
-		String strRow = "";
-		for(int i=0; i<=strArray.length();i++){
-			
-			if(i%8==0){
-				strRow = strArray.substring((i-8),i);
-				checkDuplication(strRow);
+			int row = setColumns(strArray);
+			if(row!=0){
+				result = row;
 			}
 		}
+		
+		return result;
+		
 	}
 	
-	public int checkDuplication(String nineNums){
+	public int setCell(String candidateSolution){
+		String strArray = new String(candidateSolution);
+		String strRow = "";
+		int result = 0;
+		for(int i=0; i<=strArray.length();i++){
+			
+			if(i%3==0 && i<=6){
+				strRow = strArray.substring(i,(i+3)) + strArray.substring((i+9),(i+12)) + strArray.substring((i+18),(i+21));
+				checkDuplication(strRow);
+				if(checkDuplication(strRow)==false){
+					result = -2;
+				}
+			}
+			
+		}
+		return result;
+	}
+	
+	public int setRows(String candidateSolution){
+		String strArray = new String(candidateSolution);
+		String strRow = "";
+		int result = 0;
+		for(int i=0; i<=(strArray.length()-9);i++){
+			
+			if(i%9==0){
+				strRow = strArray.substring(i,(i+9));
+				System.out.println(strRow);
+				/*checkDuplication(strRow);
+				if(checkDuplication(strRow)==false){
+					result = -3;
+				}*/
+			}
+		}
+		return result;
+	}
+	
+	public int setColumns(String candidateSolution){
+		String strArray = new String(candidateSolution);
+		String strRow = "";
+		int result = 0;
+		for(int i=0; i<=8;i++){
+			
+			if(i%9==0){
+				strRow = strArray.substring(i,(i+9));
+				System.out.println(strRow);
+				checkDuplication(strRow);
+				if(checkDuplication(strRow)==false){
+					result = -4;
+				}
+			}
+		}
+		return result;
+	}
+	
+	public boolean checkDuplication(String nineNums){
 		boolean result = false;
-		for(int i=0; i<=8; i++){
+		for(int i=0; i<nineNums.length(); i++){
 			char str = nineNums.charAt(i);
 			int num = (int)str;
-			for(int j=i+1; j<=7; j++){
+			for(int j=i+1; j<=nineNums.length(); j++){
 				char str2 = nineNums.charAt(i);
 				int num2 = (int)str2;
 				
@@ -46,10 +91,6 @@ public class SudokuVerifier {
 				}
 			}
 		}
-		if(result==true){
-			return 0;
-		}else{
-			return -1;
-		}
+		return result;
 	}
 }
--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -9,7 +9,7 @@ public class TestSudokuVerifier {
 	@Test
 	public void test() {
 		
-		String testStr = "012345678";
+		String testStr = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
 		SudokuVerifier candidate = new SudokuVerifier();
 		candidate.verify(testStr);
 		System.out.println(candidate.verify(testStr));
