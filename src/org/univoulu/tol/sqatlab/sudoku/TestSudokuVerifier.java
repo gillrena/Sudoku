fcf45d39e51aaec44de357325f68fa6171743242
gillrena <leagillrena@hotmail.com>
2015-10-02 18:38:20 +0300

    Test Sudoku functions added

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -7,12 +7,19 @@ import org.junit.Test;
 public class TestSudokuVerifier {
 
 	@Test
-	public void test() {
-		
-		String testStr = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
+	public void testCorrectSeries() {
+		String testStr = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
 		SudokuVerifier candidate = new SudokuVerifier();
-		candidate.verify(testStr);
-		System.out.println(candidate.verify(testStr));
+		int result = candidate.verify(testStr);
+		assertEquals(0,result);
+	}
+	
+	@Test
+	public void testWrongSeries() {
+		String testStrIncorrect = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
+		SudokuVerifier candidate = new SudokuVerifier();
+		int result = candidate.verify(testStrIncorrect);
+		assertEquals(-2,result);
 	}
 
 }
