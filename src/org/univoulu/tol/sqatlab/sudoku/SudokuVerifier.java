9df993dbf687a162f2cd031cae43d565cb742c7f
gillrena <leagillrena@hotmail.com>
2015-10-02 18:37:34 +0300

    Updated verifier, functions improved

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -11,26 +11,42 @@ public class SudokuVerifier {
 		if(strArray.length() != 81){
 			result = -5;
 		}else{
-			int row = setColumns(strArray);
-			if(row!=0){
-				result = row;
+			int cell = setCells(strArray);
+			int row = setRows(strArray);
+			int col = setColumns(strArray);
+			//int[] resultArray = new int [3];
+			if(cell!=0){
+				result = cell;
+			}else{
+				if(row!=0){
+					result = row;
+				}else{
+					if(col!=0){
+						result = col;
+					}
+				}
 			}
+			
+			/*for (int i=0; i<resultArray.length; i++)
+			{
+			  System.out.println("Result: "+resultArray[i]+"\n");
+			}*/
 		}
 		
 		return result;
 		
 	}
 	
-	public int setCell(String candidateSolution){
+	public int setCells(String candidateSolution){
 		String strArray = new String(candidateSolution);
-		String strRow = "";
+		String strCell = "";
 		int result = 0;
 		for(int i=0; i<=strArray.length();i++){
 			
-			if(i%3==0 && i<=6){
-				strRow = strArray.substring(i,(i+3)) + strArray.substring((i+9),(i+12)) + strArray.substring((i+18),(i+21));
-				checkDuplication(strRow);
-				if(checkDuplication(strRow)==false){
+			if(i==0||i==3||i==6||i==27||i==30||i==33||i==54||i==57||i==60){
+				strCell = strArray.substring(i,(i+3)) + strArray.substring((i+9),(i+12)) + strArray.substring((i+18),(i+21));
+				checkDuplication(strCell);
+				if(checkDuplication(strCell)==false){
 					result = -2;
 				}
 			}
@@ -47,11 +63,11 @@ public class SudokuVerifier {
 			
 			if(i%9==0){
 				strRow = strArray.substring(i,(i+9));
-				System.out.println(strRow);
-				/*checkDuplication(strRow);
+				checkDuplication(strRow);
 				if(checkDuplication(strRow)==false){
 					result = -3;
-				}*/
+					break;
+				}
 			}
 		}
 		return result;
@@ -59,16 +75,16 @@ public class SudokuVerifier {
 	
 	public int setColumns(String candidateSolution){
 		String strArray = new String(candidateSolution);
-		String strRow = "";
+		String strCol = "";
 		int result = 0;
-		for(int i=0; i<=8;i++){
+		for(int i=0; i<= strArray.length();i++){
 			
-			if(i%9==0){
-				strRow = strArray.substring(i,(i+9));
-				System.out.println(strRow);
-				checkDuplication(strRow);
-				if(checkDuplication(strRow)==false){
+			if(i<=8){
+				strCol = String.valueOf(strArray.charAt(i)+""+strArray.charAt(i+9)+""+strArray.charAt(i+18)+""+strArray.charAt(i+27)+""+strArray.charAt(i+36)+""+strArray.charAt(i+45)+""+strArray.charAt(i+54)+""+strArray.charAt(i+63)+""+strArray.charAt(i+72));
+				checkDuplication(strCol);
+				if(checkDuplication(strCol)==false){
 					result = -4;
+					break;
 				}
 			}
 		}
@@ -76,19 +92,17 @@ public class SudokuVerifier {
 	}
 	
 	public boolean checkDuplication(String nineNums){
-		boolean result = false;
-		for(int i=0; i<nineNums.length(); i++){
+		boolean result = true;
+		for(int i=0; i<(nineNums.length()-1); i++){
 			char str = nineNums.charAt(i);
-			int num = (int)str;
-			for(int j=i+1; j<=nineNums.length(); j++){
-				char str2 = nineNums.charAt(i);
-				int num2 = (int)str2;
+			for(int j=i+1; j<nineNums.length(); j++){
+				char str2 = nineNums.charAt(j);
+				//int num2 = (int)str2;
 				
-				if(num==num2){
+				if(str==str2){
 					result = false;
-				}else{
-					result = true;
 				}
+				
 			}
 		}
 		return result;
